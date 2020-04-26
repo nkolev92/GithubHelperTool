7c7857b55f05ec54c08ac9efcdafc615cbb7f5a4
Nikolche Kolev <nikolev@microsoft.com>
2020-04-26 03:45:34 -0700

    Initial implementation of the helper tool

---
+++ src/GithubHelperTool/IssueHandler.cs
@@ -0,0 +1,58 @@
+﻿using Octokit;
+
+using System;
+using System.Threading.Tasks;
+
+namespace GithubHelperTool
+{
+    internal class IssueHandler
+    {
+        private readonly GitHubClient client;
+
+        public IssueHandler(GitHubClient client)
+        {
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task CopyIssue(string fromOrganization, string fromRepo, int issueNumber, string toOrganization, string toRepo)
+        {
+            try
+            {
+                var fromIssue = await client.Issue.Get(fromOrganization, fromRepo, issueNumber);
+                NewIssue newIssue = new NewIssue(fromIssue.Title)
+                {
+                    Body = GetCopiedIssueBody(fromIssue)
+                };
+                var toIssue = await client.Issue.Create(toOrganization, toRepo, newIssue);
+                var fromComments = await client.Issue.Comment.GetAllForIssue(fromOrganization, fromRepo, issueNumber);
+                foreach (var comment in fromComments)
+                {
+                    await client.Issue.Comment.Create(toOrganization, toRepo, toIssue.Number, GetCopiedIssueCommentBody(comment));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Problem: " + e.Message);
+            }
+        }
+
+        private static string GetCopiedIssueBody(Issue issue)
+        {
+            return string.Format(Strings.CommentedOn, GetUserString(issue.User), issue.CreatedAt.ToLocalTime().ToString("R"), issue.HtmlUrl) +
+                Environment.NewLine +
+                issue.Body;
+        }
+
+        private static string GetCopiedIssueCommentBody(IssueComment issueComment)
+        {
+            return string.Format(Strings.CommentedOn, GetUserString(issueComment.User), issueComment.CreatedAt.ToLocalTime().ToString("R"), issueComment.HtmlUrl) +
+                Environment.NewLine +
+                issueComment.Body;
+        }
+
+        private static string GetUserString(User user)
+        {
+            return "@" + user.Login;
+        }
+    }
+}
---
+++ src/GithubHelperTool/Program.cs
@@ -0,0 +1,23 @@
+﻿using Octokit;
+using System;
+using System.Threading.Tasks;
+
+namespace GithubHelperTool
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Expected 1 argument (github PAT). Found " + args.Length);
+                return;
+            }
+            var client = new GitHubClient(new ProductHeaderValue("nuget-github-issue-tagger"));
+            client.Credentials = new Credentials(args[0]);
+
+            var handler = new IssueHandler(client);
+            await handler.CopyIssue("nkolev92", "FromTestRepo", 1, "nkolev92", "ToTestRepo");
+        }
+    }
+}
